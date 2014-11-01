32bb244ad1d3be170188c668661e1f35297d52ae
Lmonster <qiangixue@sina,cn>
2014-11-02 00:05:03 +0800

    Initial Commit

---
+++ 西电网址/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  西电网址
+//
+//  Created by 付国强 on 14/11/2.
+//  Copyright (c) 2014年 付国强. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 西电网址/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  西电网址
+//
+//  Created by 付国强 on 14/11/2.
+//  Copyright (c) 2014年 付国强. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
